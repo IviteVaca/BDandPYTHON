20400d980f1fa32a38595e19c152855d89d23fcf
ivite <jose.calderoncastano162@gmail.com>
2025-12-09 10:18:29 -0500

    Refactor data filtering logic and save filtered results to CSV

--- proyectoMiguel1.py
+++ proyectoMiguel1.py
@@ -2,9 +2,11 @@ import pandas as pd
 
 Datos= pd.read_csv('Datos.csv', index_col="id")
 
-DatosFiltrados = Datos [(Datos['gastos'] <= Datos['ganancias_anuales'])]
-print(DatosFiltrados)
+
 
 DatosFiltrados2= Datos [(Datos['gastos'] <= Datos['ganancias_anuales']*0.75)]   
+DatosFiltrados2= DatosFiltrados2[["gastos", "ganancias_anuales"]]
 print(DatosFiltrados2)
+
+DatosFiltrados2.to_csv('DatosFiltrados2.csv')
 
